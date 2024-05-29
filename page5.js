4e0e11b3572b65304024dccdbda29c406f08ea97
ahmedmounib2 <ahmedmounib2@gmail.com>
2024-05-29 13:24:32 +0300

    p5 edit

--- page5.js
+++ page5.js
@@ -132,15 +132,84 @@ console.log(checkSign(-2));
 
 
 // Differences between the var and let keywords
+let catName = "Quincy";
+let quote; 
 
+catName = "Beau";
 
-
-
-
-
-
-
-
-
+function catTalk() {
+  "use strict";
   
-  
+  catName = "Oliver";
+  quote = catName + " says Meow!"
+  return quote;
+} 
+
+console.log(catTalk());
+
+
+// compare scopes of the var and let keywords
+function checkScope() {
+  "use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
+
+checkScope();
+
+
+
+// Declare a Read Only Variable with the Const Keyword
+function printManyTimes(str) {
+  "use strict";
+
+  const SENTENCE = str + " is cool!";
+
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+}
+printManyTimes("Hello World!");
+
+
+// Mutate an Array Declared with const
+const s = [5, 7, 2];
+function editInPlace() {
+  "use strict";
+
+  // s = [2, 5, 7] cant re assign const , have to re assign each element as below
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+
+
+}
+editInPlace();
+
+console.log(s);
+
+// Prevent Object or Data Mutation
+function freezeObj() {
+  "use strict";
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+
+  Object.freeze(MATH_CONSTANTS); // Object.freeze prevents changes to the properties of MATH_CONSTANTS
+
+  try {
+    MATH_CONSTANTS.PI = 99; // Attempt to change the frozen property
+  } catch (ex) {
+    console.log("Error:", ex.message); // Log the error message
+  }
+  return MATH_CONSTANTS.PI; // Return the original value
+}
+
+const PI = freezeObj();
+console.log("The value of PI is:", PI); // This will log 3.14
